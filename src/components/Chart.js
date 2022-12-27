f90f066e9963af15f856668383c505307218b8da
salvagauchat <salvagauchat@gmail.com>
2022-12-26 22:15:46 -0300

    terminando sprint-8 en GRUPO

---
+++ src/components/Chart.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+
+
+
+function Chart() {
+
+    const [totalProducts, setTotalProducts] = useState({})
+
+    useEffect(() => {
+        fetch('http://localhost:3001/api/products')
+            .then(response => response.json())
+            .then((data) => {
+                //console.log(data);
+                let total = data.products
+                setTotalProducts(total)
+            })
+
+            .catch(error => (error))
+    }, [])
+    return (
+        /* <!-- DataTales Example --> */
+        <div className="card shadow mb-4">
+            <div className="card-body">
+                <div className="table-responsive">
+                    <table className="table table-bordered" id="dataTable" width="100%" cellSpacing="0">
+                        <thead>
+                            <h4>Listado de productos</h4>
+                            <tr>
+                                <th>Producto</th>
+                                <th>Genero</th>
+                                <th>Marca</th>
+                                <th>Precio</th>
+                            </tr>
+                        </thead>
+                    
+                    {totalProducts.length > 0 && totalProducts.map((product) => {
+                        return (
+                            <>
+
+                                <tr>
+                                    <th>{product.name}</th>
+                                    <th>{product.gender.name}</th>
+                                    <th>{product.brands.name}</th>
+                                    <th>{product.price}</th>
+                                </tr>
+                            </>
+                        )
+                    })}
+                </table>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Chart;
